f47b55f5b6bb6bf0a685a10dabbc480a64991353
Amangoua Ange <adagou@yahoo.com>
2023-01-25 18:20:27 +0000

    doing the header

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors:{
+      currentColor : '#131921',
+      white: '#fff',
+      gray: '#cdcdcd',
+      orange: '#febd69',
+    },
+    fontFamily:{
+      'amazonMd':['Amazon font', 'system ui']
+    },
+    extend: {},
+  },
+  plugins: [],
+  
+
+}
